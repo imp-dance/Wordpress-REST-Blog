bcb0f724c46221c07c59e672e51510842eaeb2fc
IDAbyLIGL <haakonu97@gmail.com>
2019-10-25 01:07:02 +0200

    Added build to github

---
+++ wordpress-rest-blog/build/precache-manifest.9d63f9686a8a9e72f723fbd581197636.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0591d70fa085c0e2720ab33c1fac4da5",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "43e84395a92e16f0624f",
+    "url": "/blog/static/css/main.d09d5a82.chunk.css"
+  },
+  {
+    "revision": "9ead46da5d8eadacef99",
+    "url": "/blog/static/js/2.0278bb77.chunk.js"
+  },
+  {
+    "revision": "43e84395a92e16f0624f",
+    "url": "/blog/static/js/main.a7368cfb.chunk.js"
+  },
+  {
+    "revision": "2c82435bee1a514f567a",
+    "url": "/blog/static/js/runtime~main.9c2bd09e.js"
+  }
+]);
