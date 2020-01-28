3118c756e6604d6aa5a160bcf352deb4ee950255
IDAbyLIGL <haakonu97@gmail.com>
2020-01-28 15:34:21 +0100

    Restructured project

---
+++ src/pages/Index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import WPConfig from "../WordpressConfig";
+import Articles from "../components/Articles";
+import "../App.scss";
+
+function Index() {
+  return (
+    <React.Fragment>
+      <Articles WPConfig={WPConfig}></Articles>
+    </React.Fragment>
+  );
+}
+
+export default Index;
